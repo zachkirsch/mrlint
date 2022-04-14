44962973a643b29c839870a60514d44058910b06
Zach Kirsch <zachkirsch@gmail.com>
2022-04-13 22:21:24 -0700

    Merge depcheck ignores

--- packages/rules/src/rules/depcheck.ts
+++ packages/rules/src/rules/depcheck.ts
@@ -1,4 +1,5 @@
 import { PackageType, Result, Rule, RuleType } from "@fernapi/mrlint-commons";
+import { FileSystem } from "@fernapi/mrlint-virtual-file-system";
 import produce from "immer";
 
 interface DepcheckConfig {
@@ -22,8 +23,7 @@ const FILENAME = ".depcheckrc.json";
 
 async function runRule({ fileSystems, packageToLint, logger }: Rule.PackageRuleRunnerArgs): Promise<Result> {
     let depcheckRc: DepcheckConfig = {
-        ignores: ["sass"],
-        "ignore-patterns": ["lib", ".eslintrc.json"],
+        "ignore-patterns": ["lib"],
     };
 
     if (packageToLint.config.type === PackageType.REACT_APP) {
@@ -32,16 +32,12 @@ async function runRule({ fileSystems, packageToLint, logger }: Rule.PackageRuleR
                 draft.ignores = [];
             }
             draft.ignores.push("sass");
-            if (draft["ignore-patterns"] == null) {
-                draft["ignore-patterns"] = [];
-            }
-            draft["ignore-patterns"].push("lib", ".eslintrc.json");
         });
     }
 
     const fileSystemForPackage = fileSystems.getFileSystemForPackage(packageToLint);
-
     try {
+        depcheckRc = await mergeWithExistingIgnores({ fileSystemForPackage, depcheckRc });
         await fileSystemForPackage.writeFile(FILENAME, JSON.stringify(depcheckRc));
         return Result.success();
     } catch (error) {
@@ -51,4 +47,38 @@ async function runRule({ fileSystems, packageToLint, logger }: Rule.PackageRuleR
         });
         return Result.failure();
     }
+}
+async function mergeWithExistingIgnores({
+    fileSystemForPackage,
+    depcheckRc,
+}: {
+    fileSystemForPackage: FileSystem;
+    depcheckRc: DepcheckConfig;
+}): Promise<DepcheckConfig> {
+    return produce(depcheckRc, async (draft) => {
+        const existingStr = await fileSystemForPackage.readFile(FILENAME);
+        const existing = existingStr != null ? (JSON.parse(existingStr) as DepcheckConfig) : undefined;
+        if (draft.ignores == null) {
+            draft.ignores = existing?.ignores;
+        } else {
+            if (existing?.ignores != null) {
+                for (const existingIgnore of existing.ignores) {
+                    if (!draft.ignores.includes(existingIgnore)) {
+                        draft.ignores.push(existingIgnore);
+                    }
+                }
+            }
+        }
+        if (draft["ignore-patterns"] == null) {
+            draft["ignore-patterns"] = existing?.["ignore-patterns"];
+        } else {
+            if (existing?.["ignore-patterns"] != null) {
+                for (const existingIgnorePattern of existing["ignore-patterns"]) {
+                    if (!draft["ignore-patterns"].includes(existingIgnorePattern)) {
+                        draft["ignore-patterns"].push(existingIgnorePattern);
+                    }
+                }
+            }
+        }
+    });
 }
