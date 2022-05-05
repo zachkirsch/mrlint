e53f4fb0362c549235f53d2be2fc1c5c75e53d15
Zach Kirsch <zachkirsch@gmail.com>
2022-05-05 13:54:36 -0700

    Use find-up package

--- packages/parser/src/getMonorepoRoot.ts
+++ packages/parser/src/getMonorepoRoot.ts
@@ -1,27 +1,21 @@
 import { MonorepoRoot } from "@fern-api/mrlint-commons";
-import { readdir } from "fs/promises";
-import path, { parse } from "path";
+import findUp from "find-up";
+import path from "path";
 import { z } from "zod";
 import { readConfig } from "./readConfig";
 
-const MONOREPO_ROOT_FILES = new Set([".mrlint.root.json", ".mrlint.root.yml"]);
+const MONOREPO_ROOT_FILES = [".mrlint.root.json", ".mrlint.root.yml"];
 
 const RootConfigSchema = z.strictObject({
     packages: z.string(),
     sharedConfigs: z.string(),
 });
 
-export declare namespace getMonorepoRoot {
-    export interface Args {
-        cwd?: string;
+export async function getMonorepoRoot(): Promise<MonorepoRoot> {
+    const configPath = await findUp(MONOREPO_ROOT_FILES);
+    if (configPath == null) {
+        throw new Error("Failed to find mrlint root config");
     }
-}
-
-export async function getMonorepoRoot({ cwd = process.cwd() }: getMonorepoRoot.Args = {}): Promise<MonorepoRoot> {
-    const configPath = await closest({
-        currentDirectory: cwd,
-        predicate: (filepath) => MONOREPO_ROOT_FILES.has(path.basename(filepath)),
-    });
 
     const config = await readConfig(configPath, (contents) => RootConfigSchema.parse(contents));
     if (config == null) {
@@ -32,29 +26,4 @@ export async function getMonorepoRoot({ cwd = process.cwd() }: getMonorepoRoot.A
         fullPath: path.dirname(configPath),
         config,
     };
-}
-
-async function closest({
-    currentDirectory,
-    predicate,
-}: {
-    currentDirectory: string;
-    predicate: (filepath: string) => boolean;
-}): Promise<string> {
-    const contents = await readdir(currentDirectory);
-    for (const filename of contents) {
-        const filepath = path.join(currentDirectory, filename);
-        if (predicate(filepath)) {
-            return filepath;
-        }
-    }
-
-    if (currentDirectory === parse(currentDirectory).root) {
-        throw new Error("Failed to find file");
-    }
-
-    return closest({
-        currentDirectory: path.dirname(currentDirectory),
-        predicate,
-    });
 }
