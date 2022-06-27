1bdeef8e4c60baca77c19762150d17c2f97905a6
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-06-27 16:05:43 -0700

    Add repo field to package.json (#42)

--- packages/commons/src/Rule.ts
+++ packages/commons/src/Rule.ts
@@ -41,6 +41,7 @@ export declare namespace Rule {
         allPackages: readonly Package[];
         relativePathToRoot: string;
         relativePathToSharedConfigs: string;
+        repository: string;
         fileSystems: FileSystems;
         logger: Logger;
         addDevDependency: (dependency: string) => void;
