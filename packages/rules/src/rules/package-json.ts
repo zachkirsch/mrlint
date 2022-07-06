c236d940963dff28ee27cccbaab5c41c5dd6d2ee
Zach Kirsch <zachkirsch@gmail.com>
2022-07-06 16:14:06 -0700

    compile:all before test

--- packages/rules/src/rules/package-json.ts
+++ packages/rules/src/rules/package-json.ts
@@ -228,7 +228,7 @@ function addScripts({
             }),
             {}
         ),
-        test: `yarn run compile && ${executables.get(Executable.JEST)} --passWithNoTests`,
+        test: `yarn run compile:all && ${executables.get(Executable.JEST)} --passWithNoTests`,
         "lint:eslint": `${executables.get(Executable.ESLINT)} --max-warnings 0 . --ignore-path=${pathToEslintIgnore}`,
         "lint:eslint:fix": `${executables.get(
             Executable.ESLINT
