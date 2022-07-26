712f3f9bb901c03ae4c99b6f4234eda3458d5d35
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-07-25 22:30:19 -0800

    Fix CLI publishing with empty yarn.lock (#72)
    * Try to fix publishing with empty yarn.lock
    
    * Fix exec

--- packages/rules/src/rules/cli.ts
+++ packages/rules/src/rules/cli.ts
@@ -87,12 +87,15 @@ async function main() {
 
     if (packageToLint.config.cliPackageName != null) {
         script += `
+
+    const path = require("path");
+    process.chdir(path.join(__dirname, "${ESBUILD_OUTPUT_DIR}"));
         
     // write cli's package.json
     const packageJson = require("./package.json");
     const { writeFile } = require("fs/promises");
     await writeFile(
-        "${ESBUILD_OUTPUT_DIR}/package.json",
+        "package.json",
         JSON.stringify(
             {
                 name: "${packageToLint.config.cliPackageName}",
@@ -104,7 +107,21 @@ async function main() {
             undefined,
             2
         )
-    );`;
+    );
+    
+    // write empty yarn.lock so yarn doesn't try to associate this package with the monorepo
+    await writeFile("yarn.lock", "");
+
+    // install package into new yarn.lock
+    // YARN_ENABLE_IMMUTABLE_INSTALLS=false so we can modify yarn.lock even when in CI
+    const { exec } = require("child_process");
+    exec("YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install", undefined, (error) => {
+        if (error != null) {
+            console.error(error);
+            process.exit(1);
+        }
+    })
+    `;
     }
 
     script += "\n}";
